964153c7b033fae04bd3fb7b8f759530a053ab36
Mathieu Garneau <mathieu.lemyre-garneau@mayahtt.com>
2024-04-06 16:20:17 -0400

    hello-world deployment.

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+
+def main():
+    st.set_page_config(page_title="PBtT Horaire", layout="wide")
+    st.subheader("App pour planifier jours pour PBtT")
+    st.title("my title")
+    st.write("App pour déterminer trajets quotidiens pour PBtT")
+    st.write("[Game >](https://app.roll20.net/campaigns/details/4442458/warhammer-jdr-first-edition)")
+
+
+
+if __name__ == "__main__":
+    main()
